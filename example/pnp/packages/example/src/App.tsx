d4e652787cf740c5e04b78336d57ab14b4b3865c
ronbb <ronbiaobiao@gmail.com>
2021-06-24 15:11:37 +0800

    feat(example): pnp example

---
+++ example/pnp/packages/example/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Button } from 'antd';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Button type="primary">button</Button>
+        <Button>button</Button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
