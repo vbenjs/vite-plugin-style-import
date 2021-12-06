076ca089c3f7549ad36a856b8a81f61c1cfd9cac
付慧岭 <760925513@qq.com>
2021-12-06 17:47:38 +0800

    feat: add vxe-table resolve

--- example/vue/src/main.ts
+++ example/vue/src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import VXETable from 'vxe-table'
 
 import { Button, Row, Col } from 'ant-design-vue'
 import { ElButton, ElMessage } from 'element-plus'
@@ -14,7 +15,7 @@ import {
 } from 'ant-design-vue'
 const app = createApp(App)
 
-app.use(Button).use(Row).use(Col).use(ElButton)
+app.use(Button).use(Row).use(Col).use(ElButton).use(VXETable)
 
 // eslint-disable-next-line no-console
 console.log(AntButton)
--- example/vue/vite.config.ts
+++ example/vue/vite.config.ts
@@ -1,11 +1,30 @@
 import { UserConfigExport } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import jsx from '@vitejs/plugin-vue-jsx'
-import styleImport, { AndDesignVueResolve, VantResolve, ElementPlusResolve } from '../../dist/index'
+import styleImport, {
+  AndDesignVueResolve,
+  VantResolve,
+  ElementPlusResolve,
+  VxeTableResolve,
+} from '../../dist/index'
 
 export default (): UserConfigExport => {
   return {
     css: {
+      postcss: {
+        plugins: [
+          {
+            postcssPlugin: 'internal:charset-removal',
+            AtRule: {
+              charset: (atRule) => {
+                if (atRule.name === 'charset') {
+                  atRule.remove()
+                }
+              },
+            },
+          },
+        ],
+      },
       preprocessorOptions: {
         less: {
           javascriptEnabled: true,
@@ -17,7 +36,7 @@ export default (): UserConfigExport => {
       vue(),
       jsx(),
       styleImport({
-        resolves: [AndDesignVueResolve(), VantResolve(), ElementPlusResolve()],
+        resolves: [AndDesignVueResolve(), VantResolve(), ElementPlusResolve(), VxeTableResolve()],
       }),
     ],
   }
